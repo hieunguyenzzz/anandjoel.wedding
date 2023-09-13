369925fd119feb6309e552a34966fc91179beb04
Hieu Nguyen <ngohoanglongptit@gmail.com>
2023-09-13 17:27:48 +0700

    update

---
+++ src/app/(main)/event/page.tsx
@@ -0,0 +1,46 @@
+
+import Image from '@/components/common/image'
+import creimage from './assets/CER.png'
+import recImage from './assets/REC.png'
+import bgImage from './assets/bg2.png'
+export default function Home() {
+  let ratio = (bgImage.height / bgImage.width) * 100
+  return (
+    <div className="min-h-screen text-[12px] lg:text-[2vw] relative isolate text-[#111111ff] flex flex-col gap-6">
+      <div style={{
+        minHeight: ratio + 'vw',
+      }} className='py-[18vw]  px-[18%] pb-[30vw] gap-[10vw] flex flex-col relative justify-center'>
+        <Image src={bgImage} priority placeholder='blur' fill className='absolute top-0 left-0 w-full' />
+        <div className='w-full relative  '>
+          <div className='text-center flex flex-col items-center gap-2 relative '>
+            <Image src={creimage} priority placeholder='blur' className='max-w-md w-2/5' />
+            <div className=' whitespace-pre-wrap max-w-prose leading-loose'>
+              <div className='py-1 text-[1.4em] font-bold' >at the Tree of Life</div>
+              <p>
+                {`december 16, 2023 
+03:00 – 04:30
+Thành phố Đà Lạt, Lâm Đồng, Vietnam
+(transportation will be provided to and from the venue and hotel)`}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='w-full relative  '>
+          <div className='text-center flex flex-col items-center gap-2 relative '>
+            <Image src={recImage} priority placeholder='blur' className='max-w-md w-2/5' />
+            <div className=' whitespace-pre-wrap max-w-prose leading-loose'>
+              <div className='py-1 text-[1.4em] font-bold' >at the Tree of Knowledge</div>
+              <p>
+                {`december 16, 2023
+07:00 – 10:30
+Thành phố Đà Lạt, Lâm Đồng, Vietnam
+(transportation will be provided to and from the venue and hotel)`}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='absolute top-0 left-0 w-full h-full pointer-events-none'></div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/common/image.tsx
@@ -0,0 +1,9 @@
+import NextImage, { ImageProps } from "next/image";
+
+export default function Image(props: Omit<ImageProps, 'alt'> & {
+  alt?: string
+}) {
+  return (
+    <NextImage {...props} alt={props.alt || 'image'} />
+  )
+}
