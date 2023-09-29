295c524407ca057917f6d0a77dfdeb0d3d1f1bc6
ngohoanglong <ngohoanglongptit@gmail.com>
2023-09-29 12:59:36 +0700

    update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 
+import AnimatedImage from '@/components/animateImage';
 import Image from '@/components/common/image';
 import { StaticImageData } from 'next/image';
 import Link from 'next/link';
@@ -79,12 +80,30 @@ export const menuItemsL: {
     },
   ]
 export default function Page() {
+  const containerId = "containerId"
+  const innerId = "innerId"
   return (
-    <div className="animate-fade w-full animate-duration-[3000ms] animate-ease-in-out text-[12px] lg:text-lg  isolate text-[#111111ff] flex flex-col gap-6 relative isolate">
-      <div className=' container mx-auto flex flex-col relative justify-center items-center lg:relative'>
-        <Image src={bgImage} priority placeholder='blur' className='hidden  lg:block -z-10 inset-0 w-full  max-w-full object-cover animate-fade' />
+    <div className="animate-fade w-full animate-duration-[3000ms] animate-ease-in-out text-[12px] lg:text-lg text-[#111111ff] flex flex-col gap-6 relative isolate">
+      <div id={containerId} className='overflow-hidden container mx-auto flex flex-col relative justify-center items-center lg:relative '>
         <Image src={bgImageMoble} priority placeholder='blur' className=' -z-10 lg:hidden inset-0 fixed w-full h-full max-w-full object-cover animate-fade' />
-        <ul className='justify-center w-full animate-fade origin-top-right duration-300 text-center min-h-[calc(100vh-280px)] flex-col   isolate  z-10 top-0 left-0 flex  w-full items-center gap-6 lg:hidden'>
+        <div id={innerId} className='transition-transform duration-300 ease-in-out'>
+          <Image src={bgImage} priority placeholder='blur' className='hidden transition-transform lg:block -z-10 inset-0 w-full  max-w-full object-cover animate-fade' />
+          <ul className='hidden lg:block absolute inset-0 w-full h-full'>
+            {
+              menuItemsL.map((item, index) => {
+                return <li key={index} >
+                  <Link href={item.href} style={item.style} id={item.title} className='absolute '>
+                    <Image style={{
+                      animationDelay: `${300 + (index + 1) * 150}ms`
+                    }} width={200} height={200} src={item.image} alt={item.title} className='object-cover w-full absolute opacity-0 animate-fade-up hover:scale-110 duration-500  ease-in-out  transition-transform' />
+                    <div className='text-2xl font-semibold w-full uppercase hidden'>{item.title}</div>
+                  </Link>
+                </li>
+              })}
+
+          </ul>
+        </div>
+        <ul className='justify-center w-full animate-fade origin-top-right duration-300 text-center min-h-[calc(100vh-280px)] flex-col   isolate  z-10 top-0 left-0 flex items-center gap-6 lg:hidden'>
           {
             menuItems.map((item, index) => {
               if (item.href === '/') {
@@ -100,22 +119,10 @@ export default function Page() {
               </li>
             })}
         </ul>
-        <ul className='hidden lg:block absolute inset-0 w-full h-full'>
-          {
-            menuItemsL.map((item, index) => {
-              return <li key={index} >
-                <Link href={item.href} style={item.style} id={item.title} className='absolute '>
-                  <Image style={{
-                    animationDelay: `${300 + (index + 1) * 150}ms`
-                  }} width={200} height={200} src={item.image} alt={item.title} className='object-cover w-full absolute opacity-0 animate-fade-up hover:scale-110 duration-500  ease-in-out  transition-transform' />
-                  <div className='text-2xl font-semibold w-full uppercase hidden'>{item.title}</div>
-                </Link>
-              </li>
-            })}
 
-        </ul>
         <div className="absolute inset-0 hidden lg:block w-full h-full pointer-events-none" style={{ boxShadow: 'inset 0px 0px 16px 16px #C788B0, inset 0px 0px 33px 27px #C788B0' }} />
       </div>
+      <AnimatedImage containerId={containerId} innerId={innerId} />
     </div>
   )
 }
