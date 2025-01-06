cba03cd6a47148e958bf35c4724f1400481317fd
long ngo haong <ngohoanglongptit@gmail.com>
2025-01-06 18:59:33 +0700

    updaate

--- src/components/common/image.tsx
+++ src/components/common/image.tsx
@@ -1,3 +1,4 @@
+"use client"
 import NextImage, { ImageProps, } from "next/image";
 
 export default function Image({...props}: Omit<ImageProps, 'alt' | 'src'> & {
@@ -6,16 +7,15 @@ export default function Image({...props}: Omit<ImageProps, 'alt' | 'src'> & {
   animated?: boolean,
 }) {
   const { style, className, animated, src = "/", ...rest } = props
-  let _src =src
-  if(typeof _src === 'string'){
-    //  _src = props?.src?.replace("https://assets.tina.io/3a743d97-a554-4b19-acc5-85219789c469","https://res.cloudinary.com/dfgbpib38/image/upload/f_auto,w_2160/anandjoeltina")
-    _src =src
+  let useLoader =false
+  if(typeof src === 'string'&& src.includes("https://assets.tina.io/3a743d97-a554-4b19-acc5-85219789c469")){
+    useLoader=true
   }
   return (
-    <NextImage {...rest} alt={props.alt || 'image'} src={_src || '/'}
-      loader={({src,width})=>{
+    <NextImage {...rest} alt={props.alt || 'image'} src={src || '/'}
+      loader={useLoader?({src,width})=>{
         return `https://thumbor.hieunguyen.dev/unsafe/${width}x/`+encodeURIComponent(src)
-      }}
+      }:undefined}
       className={className}
     />
   )
