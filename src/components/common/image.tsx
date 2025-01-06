64f3d76f4ab4a9c6d8b4fe6f85988d3568ca72f4
ngohoanglong <ngohoanglongptit@gmail.com>
2025-01-06 11:58:44 +0700

    update

--- src/components/common/image.tsx
+++ src/components/common/image.tsx
@@ -6,7 +6,9 @@ export default function Image({...props}: Omit<ImageProps, 'alt' | 'src'> & {
   animated?: boolean,
 }) {
   const { style, className, animated, src = "/", ...rest } = props
-  let _src = props?.src?.replace("https://assets.tina.io/3a743d97-a554-4b19-acc5-85219789c469","https://res.cloudinary.com/dfgbpib38/image/upload/f_auto,w_2160/anandjoeltina")
+let _src =src
+if(typeof _src === 'string')
+   _src = props?.src?.replace("https://assets.tina.io/3a743d97-a554-4b19-acc5-85219789c469","https://res.cloudinary.com/dfgbpib38/image/upload/f_auto,w_2160/anandjoeltina")
   return (
     <NextImage {...rest} alt={props.alt || 'image'} src={_src || '/'}
       className={className}
